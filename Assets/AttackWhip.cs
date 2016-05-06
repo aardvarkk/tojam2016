a5d4dad037b362e97adb519530a3605eeb223b85
Ian Clarkson <clarksonian@gmail.com>
2016-05-06 18:10:49 -0400

    Cannot whip too quickly after an existing whip

--- Assets/AttackWhip.cs
+++ Assets/AttackWhip.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class AttackWhip : MonoBehaviour {
 
+	public static float AttackDuration = 0.5f;
+	float LastAttack = 0;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,29 +13,40 @@ public class AttackWhip : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("Whip")) {
-			// Measure the attack direction - if it's invalid, attack forward
-			float x = Input.GetAxisRaw("Horizontal");
-			float y = Input.GetAxisRaw ("Vertical");
 
-			// Default attack north if no keys are being held
-			Compass.Direction dir;
-			if (x == 0 && y == 0) {
-				dir = Compass.Direction.N;
-			} else {
-				dir = Compass.GetDirection (new Vector3 (x, y, 0));
-			}
+		// Not trying to whip, so we don't care!
+		if (!Input.GetButtonDown ("Whip"))
+			return;
 
-			// Don't allow attacks behind you
-			switch (dir) {
-			case Compass.Direction.SW:
-			case Compass.Direction.S:
-			case Compass.Direction.SE:
-				dir = Compass.Direction.N;
-				break;
-			}
-
-			Debug.Log ("Whip " + dir.ToString());
+		// Attacked too recently, so ignore
+		if (LastAttack + AttackDuration >= Time.timeSinceLevelLoad) {
+			Debug.Log ("Ignoring whip! Wait a bit, asshole!");
+			return;
 		}
+		
+		// Measure the attack direction - if it's invalid, attack forward
+		float x = Input.GetAxisRaw("Horizontal");
+		float y = Input.GetAxisRaw ("Vertical");
+
+		// Default attack north if no keys are being held
+		Compass.Direction dir;
+		if (x == 0 && y == 0) {
+			dir = Compass.Direction.N;
+		} else {
+			dir = Compass.GetDirection (new Vector3 (x, y, 0));
+		}
+
+		// Don't allow attacks behind you
+		switch (dir) {
+		case Compass.Direction.SW:
+		case Compass.Direction.S:
+		case Compass.Direction.SE:
+			dir = Compass.Direction.N;
+			break;
+		}
+
+		LastAttack = Time.timeSinceLevelLoad;
+
+		Debug.Log ("Whip " + dir.ToString());
 	}
 }
