e457e2447d54b95b5b911561d791244b6c8b6a7a
Ian Clarkson <clarksonian@gmail.com>
2016-05-07 18:51:47 -0400

    Boss is moving reasonably (and starts moving once the camera catches up)

---
+++ Assets/BossMover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossMover : MonoBehaviour {
+
+	public float TopBuffer;
+
+	void Update () {
+
+		float halfHeight = GetComponent<SpriteRenderer> ().bounds.extents.y;
+
+		// Don't allow ourselves to get less than the camera (stick us to the top once we're found)
+		transform.position = new Vector3(
+			transform.position.x,
+			Mathf.Max(transform.position.y, Camera.main.transform.position.y + Global.ScreenDimension / 2 - halfHeight - TopBuffer),
+			transform.position.z
+		);
+	}
+}
