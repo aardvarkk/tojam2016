c5353c16f42b334a003ca1a27e0f1c0f06349a49
Ian Clarkson <clarksonian@gmail.com>
2016-05-08 17:05:17 -0400

    Fireball sound

---
+++ Assets/Fireball.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fireball : MonoBehaviour {
+
+	public AudioClip FireballSound;
+
+	// Use this for initialization
+	void Start () {
+		GameObject.Find ("SoundPlayer").GetComponent<AudioSource> ().PlayOneShot (FireballSound);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
