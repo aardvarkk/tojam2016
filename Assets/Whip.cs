8c41d39906268c28d61bcbbdb83fe8cbaa3373fc
Ian Clarkson <clarksonian@gmail.com>
2016-05-08 00:57:43 -0400

    Fixing not being able to whip villagers

--- Assets/Whip.cs
+++ Assets/Whip.cs
@@ -25,9 +25,9 @@ public class Whip : MonoBehaviour {
 //		Debug.Log (string.Format ("Reduced health to {0}", e.Health));
 
 		// If we can reduce the health bar, do it!
-		Transform t = other.transform.parent.Find("HealthBarOverlay").Find("HealthBarPivot");
+		Transform t = other.transform.parent.Find("HealthBarOverlay");
 		if (t)
-			t.localScale = new Vector3(e.Health / e.StartHealth, 1, 1);
+			t.Find("HealthBarPivot").localScale = new Vector3(e.Health / e.StartHealth, 1, 1);
 
 		// If we killed it, great!
 		if (e.Health <= 0) 
